6284d4549ee58a96d96b4cdbdeeb5b6434aa2e9f
mattthewaz <mattthewaz@gmail.com>
2019-04-06 13:48:24 -0400

    Step complete - Standalone application will look for the presesence of an ini file containing server settings and read in the values. If an error is encountered, display to the user and abort application.

--- MultiplayerDedicatedServer/Program.cs
+++ MultiplayerDedicatedServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MultiplayerDedicatedServer.Util;
 
 namespace MultiplayerDedicatedServer
 {
@@ -10,6 +11,20 @@ namespace MultiplayerDedicatedServer
     {
         static void Main(string[] args)
         {
+            // Step: Read in settings from INI file
+            var settings = SettingsReader.ReadServerSettings();
+
+            if (settings == null)
+            {
+                Logger.Write("Server settings could not be read", Logger.MessageType.Fatal);
+                return;
+            }
+
+            // Step: Allow user to select a file (or quit)
+
+            // Step: Start server on different thread
+
+            // Step: Listen for console commands
         }
     }
 }
---
+++ MultiplayerDedicatedServer/Util/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerDedicatedServer.Util
+{
+    // Class for logging to be hooked up. Just a stub for now.
+    public static class Logger
+    {
+        public enum MessageType
+        {
+            Message = 0,
+            Info = 1,
+            Warning = 2,
+            Error = 3,
+            Fatal = 4
+        };
+
+
+        public static void Write(string message, MessageType type)
+        {
+            switch (type)
+            {
+                case MessageType.Info:
+                    Console.WriteLine($"[Info] - {message}");
+                    break;
+                case MessageType.Warning:
+                    Console.WriteLine($"[Warn] - {message}");
+                    break;
+                case MessageType.Error:
+                    Console.WriteLine($"[Error] - {message}");
+                    break;
+                case MessageType.Fatal:
+                    Console.WriteLine($"[FATAL] - {message}");
+                    break;
+                default:
+                    Console.WriteLine(message);
+                    break;
+            }
+        }
+    }
+}
